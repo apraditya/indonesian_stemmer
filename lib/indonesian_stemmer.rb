dfa33c82748a3cb3f45724102a04d38c3d6feb59
Adinda Praditya <apraditya@gmail.com>
2013-03-25 19:00:03 +0800

    Added #remove_particle method

--- lib/indonesian_stemmer.rb
+++ lib/indonesian_stemmer.rb
@@ -1,14 +1,29 @@
 require "indonesian_stemmer/version"
+require "lucene_analysis/stemmer_utility"
 
 module IndonesianStemmer
   VOWEL_CHARACTERS    = %w( a e i o u )
+  PARTICLE_CHARACTERS = %w( kah lah pun )
+
   class << self
+    include LuceneAnalysis::StemmerUtility
+    attr_accessor :number_of_syllables
+
     def total_syllables(word)
       result = 0
       word.size.times do |i|
         result += 1 if is_vowel?(word[i])
       end
       result
+    end
+
+    def remove_particle(word)
+      @number_of_syllables ||= total_syllables(word)
+      if PARTICLE_CHARACTERS.any? { |char| ends_with?(word, word.size, char) }
+        @number_of_syllables -= 1
+        word.slice!( -3, 3)
+      end
+      word
     end
 
 
--- spec/indonesian_stemmer/indonesian_stemmer_spec.rb
+++ spec/indonesian_stemmer/indonesian_stemmer_spec.rb
@@ -14,4 +14,45 @@ describe IndonesianStemmer do
       IndonesianStemmer.total_syllables('permainan').should == 4
     end
   end
+
+  describe '#remove_particle' do
+    describe 'should remove the particles at the end of the word' do
+      it "'kah'" do
+        should_transform(:remove_particle, 'manakah', 'mana')
+      end
+
+      it "'lah'" do
+        should_transform(:remove_particle, 'kembalilah', 'kembali')
+      end
+
+      it "'pun'" do
+        should_transform(:remove_particle, 'bagaimanapun', 'bagaimana')
+      end
+    end
+
+    describe 'should not remove the particles at the rest part of the word' do
+      it "'kah'" do
+        should_not_transform(:remove_particle, 'kahak')
+        should_not_transform(:remove_particle, 'pernikahan')
+      end
+
+      it "'lah'" do
+        should_not_transform(:remove_particle, 'lahiriah')
+        should_not_transform(:remove_particle, 'kelahiran')
+      end
+
+      it "'pun'" do
+        should_not_transform(:remove_particle, 'punya')
+        should_not_transform(:remove_particle, 'kepunyaan')
+      end
+    end
+  end
+end
+
+def should_transform(method_name, word, transformed_word)
+  IndonesianStemmer.send(method_name, word).should == transformed_word
+end
+
+def should_not_transform(method_name, word)
+  IndonesianStemmer.send(method_name, word).should == word
 end
