604734502e349702aab366f614b92e9949e1c67e
Adinda Praditya <apraditya@gmail.com>
2013-03-25 19:46:20 +0800

    Added #remove_possessive_pronoun

--- lib/indonesian_stemmer.rb
+++ lib/indonesian_stemmer.rb
@@ -2,8 +2,9 @@ require "indonesian_stemmer/version"
 require "lucene_analysis/stemmer_utility"
 
 module IndonesianStemmer
-  VOWEL_CHARACTERS    = %w( a e i o u )
-  PARTICLE_CHARACTERS = %w( kah lah pun )
+  VOWEL_CHARACTERS              = %w( a e i o u )
+  PARTICLE_CHARACTERS           = %w( kah lah pun )
+  POSSESSIVE_PRONOUN_CHARACTERS = %w( ku mu nya )
 
   class << self
     include LuceneAnalysis::StemmerUtility
@@ -19,17 +20,30 @@ module IndonesianStemmer
 
     def remove_particle(word)
       @number_of_syllables ||= total_syllables(word)
-      if PARTICLE_CHARACTERS.any? { |char| ends_with?(word, word.size, char) }
-        @number_of_syllables -= 1
-        word.slice!( -3, 3)
-      end
-      word
+      remove_characters_matching_collection(word, :particle, :end)
+    end
+
+    def remove_possessive_pronoun(word)
+      @number_of_syllables ||= total_syllables(word)
+      remove_characters_matching_collection(word, :possessive_pronoun, :end)
     end
 
 
     private
       def is_vowel?(character)
         VOWEL_CHARACTERS.include? character
+      end
+
+      def remove_characters_matching_collection(word, type, position)
+        collection = const_get("#{type}_characters".upcase.to_sym)
+        chars_to_remove = collection.select { |c| send("#{position}s_with?", word, word.size, c) }.first
+        if chars_to_remove
+          @number_of_syllables -= 1
+          chars_length = chars_to_remove.size
+          multiplier = (position == 'start')? 0 : -1
+          word.slice!( multiplier*chars_length, chars_length)
+        end
+        word
       end
   end
 end
--- spec/indonesian_stemmer/indonesian_stemmer_spec.rb
+++ spec/indonesian_stemmer/indonesian_stemmer_spec.rb
@@ -16,7 +16,7 @@ describe IndonesianStemmer do
   end
 
   describe '#remove_particle' do
-    describe 'should remove the particles at the end of the word' do
+    describe 'should remove these particles at the end of the word' do
       it "'kah'" do
         should_transform(:remove_particle, 'manakah', 'mana')
       end
@@ -30,7 +30,7 @@ describe IndonesianStemmer do
       end
     end
 
-    describe 'should not remove the particles at the rest part of the word' do
+    describe 'should not remove these particles at the rest part of the word' do
       it "'kah'" do
         should_not_transform(:remove_particle, 'kahak')
         should_not_transform(:remove_particle, 'pernikahan')
@@ -44,6 +44,39 @@ describe IndonesianStemmer do
       it "'pun'" do
         should_not_transform(:remove_particle, 'punya')
         should_not_transform(:remove_particle, 'kepunyaan')
+      end
+    end
+  end
+
+  describe '#remove_possessive_pronoun' do
+    describe 'should remove these possessive pronouns at the end of the word' do
+      it "'ku'" do
+        should_transform(:remove_possessive_pronoun, 'mainanku', 'mainan')
+      end
+
+      it "'mu'" do
+        should_transform(:remove_possessive_pronoun, 'mobilmu', 'mobil')
+      end
+
+      it "'nya'" do
+        should_transform(:remove_possessive_pronoun, 'gelasnya', 'gelas')
+      end
+    end
+
+    describe 'should not remove these possessive pronouns at the rest part of the word' do
+      it "'ku'" do
+        should_not_transform(:remove_possessive_pronoun, 'kumakan')
+        should_not_transform(:remove_possessive_pronoun, 'kekurangan')
+      end
+
+      it "'mu'" do
+        should_not_transform(:remove_possessive_pronoun, 'murahan')
+        should_not_transform(:remove_possessive_pronoun, 'kemurkaan')
+      end
+
+      it "'nya'" do
+        should_not_transform(:remove_possessive_pronoun, 'nyapu')
+        should_not_transform(:remove_possessive_pronoun, 'menyambung')
       end
     end
   end
