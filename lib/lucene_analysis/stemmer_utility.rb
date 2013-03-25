9dc22c8a4db4ecece0f33c85d48eda338ddef007
Adinda Praditya <apraditya@gmail.com>
2013-03-25 08:10:49 +0800

    ported stemmer utility

---
+++ lib/lucene_analysis/stemmer_utility.rb
@@ -0,0 +1,27 @@
+module LuceneAnalysis
+  module StemmerUtility
+
+    def self.included(receiver)
+      receiver.send :include, InstanceMethods
+    end
+
+    module InstanceMethods
+      def starts_with?(text, by_text_size, prefix)
+        return false if prefix.size > by_text_size
+        prefix.size.times do |i|
+          return false if text[i] != prefix[i]
+        end
+        return true
+      end
+
+      def ends_with?(text, by_text_size, suffix)
+        suffix_size = suffix.size
+        return false if suffix_size > by_text_size
+        suffix_size.times do |i|
+          return false if text[0 - (suffix_size - i)] != suffix[i]
+        end
+        return true
+      end
+    end
+  end
+end
---
+++ spec/lucene_analysis/stemmer_utility_spec.rb
@@ -0,0 +1,60 @@
+require 'spec_helper'
+require 'lucene_analysis/stemmer_utility'
+
+describe LuceneAnalysis::StemmerUtility do
+  before do
+    class AClass
+      include LuceneAnalysis::StemmerUtility
+    end
+    @an_object = AClass.new
+    @word = 'asldkamsdo'
+  end
+
+  describe "the word #starts_with? prefix" do
+    it 'that have different letters from the prefix should return false' do
+      @an_object.starts_with?(@word, 3, 'ber').should be_false
+    end
+
+    describe "that have the same letters with the word's first letters" do
+      before do
+        @prefix = 'asld'
+      end
+
+      it "by exactly prefix length should be true" do
+        @an_object.starts_with?(@word, @prefix.size, @prefix).should be_true
+      end
+
+      it "by more than prefix length should still be true" do
+        @an_object.starts_with?(@word, @prefix.size+1, @prefix).should be_true
+      end
+
+      it "by less than prefix length should be false" do
+        @an_object.starts_with?(@word, @prefix.size-1, @prefix).should be_false
+      end
+    end
+  end
+
+  describe "the word #ends_with? suffix" do
+    it 'that have different letters from the suffix should return false' do
+      @an_object.ends_with?(@word, 3, 'abc').should be_false
+    end
+
+    describe "that have the same letters with the word's last letters" do
+      before do
+        @suffix = 'amsdo'
+      end
+
+      it "by exactly suffix length should be true" do
+        @an_object.ends_with?(@word, @suffix.size, @suffix).should be_true
+      end
+
+      it "by more than suffix length should still be true" do
+        @an_object.ends_with?(@word, @suffix.size+1, @suffix).should be_true
+      end
+
+      it "by less than suffix length should be false" do
+        @an_object.ends_with?(@word, @suffix.size-1, @suffix).should be_false
+      end
+    end
+  end
+end
