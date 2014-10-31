5493eb1383baccc257dd04b3c5e53fa77d252cfd
Nugroho Herucahyono <xinuc@xinuc.org>
2014-10-31 17:04:42 +0700

    fix spec, use new be_falsey & be_truthy syntax

--- spec/lib/indonesian_stemmer/stemmer_utility_spec.rb
+++ spec/lib/indonesian_stemmer/stemmer_utility_spec.rb
@@ -11,7 +11,7 @@ describe IndonesianStemmer::StemmerUtility do
 
   describe "the word #starts_with? prefix" do
     it 'that have different letters from the prefix should return false' do
-      @an_object.starts_with?(@word, 3, 'ber').should be_false
+      @an_object.starts_with?(@word, 3, 'ber').should be_falsey
     end
 
     describe "that have the same letters with the word's first letters" do
@@ -20,22 +20,22 @@ describe IndonesianStemmer::StemmerUtility do
       end
 
       it "by exactly prefix length should be true" do
-        @an_object.starts_with?(@word, @prefix.size, @prefix).should be_true
+        @an_object.starts_with?(@word, @prefix.size, @prefix).should be_truthy
       end
 
       it "by more than prefix length should still be true" do
-        @an_object.starts_with?(@word, @prefix.size+1, @prefix).should be_true
+        @an_object.starts_with?(@word, @prefix.size+1, @prefix).should be_truthy
       end
 
       it "by less than prefix length should be false" do
-        @an_object.starts_with?(@word, @prefix.size-1, @prefix).should be_false
+        @an_object.starts_with?(@word, @prefix.size-1, @prefix).should be_falsey
       end
     end
   end
 
   describe "the word #ends_with? suffix" do
     it 'that have different letters from the suffix should return false' do
-      @an_object.ends_with?(@word, 3, 'abc').should be_false
+      @an_object.ends_with?(@word, 3, 'abc').should be_falsey
     end
 
     describe "that have the same letters with the word's last letters" do
@@ -44,15 +44,15 @@ describe IndonesianStemmer::StemmerUtility do
       end
 
       it "by exactly suffix length should be true" do
-        @an_object.ends_with?(@word, @suffix.size, @suffix).should be_true
+        @an_object.ends_with?(@word, @suffix.size, @suffix).should be_truthy
       end
 
       it "by more than suffix length should still be true" do
-        @an_object.ends_with?(@word, @suffix.size+1, @suffix).should be_true
+        @an_object.ends_with?(@word, @suffix.size+1, @suffix).should be_truthy
       end
 
       it "by less than suffix length should be false" do
-        @an_object.ends_with?(@word, @suffix.size-1, @suffix).should be_false
+        @an_object.ends_with?(@word, @suffix.size-1, @suffix).should be_falsey
       end
     end
   end
